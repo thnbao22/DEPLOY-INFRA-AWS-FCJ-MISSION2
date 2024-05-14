50009b9807a01dbab0a8c7173a0fa4ccee0925e3
Trương Trần Thiên Bảo <baopk22092004@gmail.com>
2024-05-14 13:04:28 +0700

    Add explaination about variable

--- modules/Networking/variables.tf
+++ modules/Networking/variables.tf
@@ -1,7 +1,8 @@
 variable "availabitity_zones" {
   description = "AZs in this region to use"
-  default = ["ap-southeast-1a", "ap-southeast-1b"]
+  default     = ["ap-southeast-1a", "ap-southeast-1b"]
 }
+# Input CIDR Block for the VPC
 variable "cidr_block" {
   
 }
