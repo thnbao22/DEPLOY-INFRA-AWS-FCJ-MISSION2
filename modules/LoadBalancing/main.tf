08ac4fd4c738285c18f8e9eea70bb5e57c2f7295
Trương Trần Thiên Bảo <baopk22092004@gmail.com>
2024-05-14 13:07:20 +0700

    Add explaination about variable

--- modules/LoadBalancing/main.tf
+++ modules/LoadBalancing/main.tf
@@ -1,3 +1,4 @@
+# Define Load Balancer resources
 resource "aws_lb" "application_load_balancer" {
   name                = "ALB-Web-Server"
   internal            = false
@@ -5,7 +6,7 @@ resource "aws_lb" "application_load_balancer" {
   security_groups     = [ var.alb_security_group_id ]
   subnets             = [ var.public_subnet_1_id, var.public_subnet_2_id ]
 }
-
+# Target Group 
 resource "aws_lb_target_group" "instance_tg" {
   name      = "ASG-Web-Server-Target-Group"
   port      = var.port
@@ -13,12 +14,11 @@ resource "aws_lb_target_group" "instance_tg" {
   vpc_id    = var.one_tier_vpc_id
 }
 
-
+# Listener
 resource "aws_lb_listener" "application_load_balancer_listener" {
   load_balancer_arn = aws_lb.application_load_balancer.arn
   port              = var.port
   protocol          = var.protocol
-
   default_action {
     type              = "forward"
     target_group_arn  = aws_lb_target_group.instance_tg.arn
